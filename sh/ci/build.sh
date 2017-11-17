baf907f274007767e06a956a4282010487717241
Ryan Van Oss <ryan.vanoss@gmail.com>
2017-11-16 20:58:30 -0800

    ci(travis) Add build.sh script.

---
+++ sh/ci/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# recommended by travis-ci
+# @see https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
+set -ev
+
+# @see https://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKER_UTIL_PATH=../../docker-util
+
+
+docker build --tag $DOCKER_IMAGE_NAME --compress --force-rm --squash .
+$DOCKER_UTIL_PATH/sh/test-image-size.sh -i $DOCKER_IMAGE_NAME -t $IMAGE_SIZE_THRESHOLD
