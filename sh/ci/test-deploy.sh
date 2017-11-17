bb1a2dd9f6bee53e81d9011c679e080052d46ff7
Ryan Van Oss <ryan.vanoss@gmail.com>
2017-11-16 21:36:20 -0800

    ci(travis): Add "Stage Image" build stage.

--- sh/ci/test-deploy.sh
+++ sh/ci/test-deploy.sh
@@ -5,7 +5,12 @@
 # @see https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
 set -ev
 
+# @see https://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKER_UTIL_PATH=$DIR../../docker-util
 IMAGE_WITH_TAG=$DOCKER_PUBLIC_IMAGE_NAME:$TAG
+
+
 
 docker pull $IMAGE_WITH_TAG
 docker run --rm $IMAGE_WITH_TAG --version
